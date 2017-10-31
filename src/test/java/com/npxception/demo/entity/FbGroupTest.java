9a60208ae41dae604d037274a343b337f2733345
RachelYing <33263585+RachelYing@users.noreply.github.com>
2017-10-31 11:57:58 -0700

    Add files via upload

---
+++ src/test/java/com/npxception/demo/entity/FbGroupTest.java
@@ -0,0 +1,44 @@
+package com.npxception.demo.entity;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FbGroupTest {
+  @Before
+  public void setUp() throws Exception {
+
+  }
+
+  @Test
+  public void getGroupID() throws Exception {
+
+  }
+
+  @Test
+  public void setGroupID() throws Exception {
+
+  }
+
+  @Test
+  public void getAdmin() throws Exception {
+
+  }
+
+  @Test
+  public void setAdmin() throws Exception {
+
+  }
+
+  @Test
+  public void getName() throws Exception {
+
+  }
+
+  @Test
+  public void setName() throws Exception {
+
+  }
+
+}
---
+++ src/test/java/com/npxception/demo/entity/PostTest.java
@@ -0,0 +1,79 @@
+package com.npxception.demo.entity;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PostTest {
+  private Post post1;
+  private Post mt;
+  private Post newPost;
+  @Before
+  public void setUp() throws Exception {
+    this.post1 = new Post("Sally","Smith", "Hello World", 5, 12, 1,"a.b");
+    this.mt = new Post();
+    this.newPost = new Post("Joe", "Apple","a", 37, 37, 37,"c.d");
+  }
+
+  @Test
+  public void testsForAllSetters() throws Exception {
+    this.mt.setId(37);
+    this.mt.setAuthorFirstName("Joe");
+    this.mt.setAuthorLastName("Joe");
+
+    this.mt.setContent("a");
+    this.mt.setLikes(37);
+    this.mt.setTime(37);
+    this.mt.setVisibility(37);
+    Assert.assertEquals(mt, newPost);
+  }
+
+  @Test
+  public void getId() throws Exception {
+    Assert.assertEquals(post1.getId(), 1);
+
+  }
+
+
+  @Test
+  public void getAuthorFirstName() throws Exception {
+    Assert.assertEquals(post1.getAuthorFirstName(), "Sally");
+  }
+
+
+  @Test
+  public void getAuthorLastName() throws Exception {
+    Assert.assertEquals(post1.getAuthorFirstName(), "Smith");
+  }
+
+
+  @Test
+  public void getContent() throws Exception {
+    Assert.assertEquals(post1.getContent(),"Hello World");
+  }
+
+
+  @Test
+  public void getLikes() throws Exception {
+    Assert.assertEquals(post1.getLikes(), 5);
+  }
+
+
+
+
+  @Test
+  public void getTime() throws Exception {
+    Assert.assertEquals(post1.getTime(), 12);
+
+  }
+
+  @Test
+  public void getVisibility() throws Exception {
+    Assert.assertEquals(post1.getVisibility(), 1);
+  }
+
+
+
+}
